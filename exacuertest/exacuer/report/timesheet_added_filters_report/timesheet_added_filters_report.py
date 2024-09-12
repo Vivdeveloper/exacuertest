b100db3285bc4ff9869d254b88f2914d6323faa1
Sushant <sushantmanjare33@gmail.com>
2024-09-12 19:34:00 +0530

    added sales count

--- exacuertest/exacuer/report/timesheet_added_filters_report/timesheet_added_filters_report.py
+++ exacuertest/exacuer/report/timesheet_added_filters_report/timesheet_added_filters_report.py
@@ -12,11 +12,9 @@ def execute(filters=None):
 
 	total_hours = sum(float(row[2]) for row in data)
 
-	
 	total_hours = "{:.2f}".format(total_hours)
 
-	
-	total_row = ["", _("Total"), total_hours, "", "", "", ""]
+	total_row = ["", _("Total"), total_hours, "", "", "", "", ""]
 	data.append(total_row)
 
 	return columns, data
@@ -26,23 +24,25 @@ def get_columns():
 		_("Employee") + "::150",
 		_("Employee Name") + "::150",
 		_("Total Hours") + "::70",
+		_("Sales Count") + "::70",  
 		_("Project") + ":Link/Project:120",
 		_("Project Name") + "::150",  
 		_("Customer") + ":Link/Customer:150",
-		_("Custom Type") + "::120"  
+		_("Custom Type") + "::120"
 	]
 
 def get_data(conditions, filters):
 	time_sheet = frappe.db.sql(
 		"""select `tabTimesheet`.employee, `tabTimesheet`.employee_name,
-		SUM(`tabTimesheet Detail`.hours) as total_hours, `tabTimesheet Detail`.project,
-		`tabProject`.project_name, `tabTimesheet`.customer, `tabTimesheet`.custom_type
+		SUM(`tabTimesheet Detail`.hours) as total_hours, `tabTimesheet Detail`.custom_sales_count,  
+		`tabTimesheet Detail`.project, `tabProject`.project_name, `tabTimesheet`.customer, 
+		`tabTimesheet`.custom_type
 		from `tabTimesheet Detail`
 		join `tabTimesheet` on `tabTimesheet Detail`.parent = `tabTimesheet`.name
 		join `tabProject` on `tabTimesheet Detail`.project = `tabProject`.name
 		where %s
 		group by `tabTimesheet`.employee, `tabTimesheet Detail`.project, `tabProject`.project_name, 
-		`tabTimesheet`.customer, `tabTimesheet`.custom_type
+		`tabTimesheet`.customer, `tabTimesheet`.custom_type, `tabTimesheet Detail`.custom_sales_count  
 		order by `tabTimesheet`.employee""" % (conditions),
 		filters,
 		as_list=1,
@@ -56,12 +56,10 @@ def get_data(conditions, filters):
 def get_conditions(filters):
 	conditions = "`tabTimesheet`.docstatus = 1"
 	
-	
 	if filters.get("start_date"):
 		conditions += " and `tabTimesheet Detail`.from_time >= %(start_date)s"
 	if filters.get("end_date"):
 		conditions += " and `tabTimesheet Detail`.to_time <= %(end_date)s"
-	
 	
 	if filters.get("employee"):
 		conditions += " and `tabTimesheet`.employee = %(employee)s"
@@ -69,7 +67,6 @@ def get_conditions(filters):
 		conditions += " and `tabTimesheet Detail`.project = %(project)s"
 	if filters.get("customer"):
 		conditions += " and `tabTimesheet`.customer = %(customer)s"
-
 	if filters.get("custom_type") is not None:
 		conditions += " and `tabTimesheet`.custom_type = %(custom_type)s"
 	
